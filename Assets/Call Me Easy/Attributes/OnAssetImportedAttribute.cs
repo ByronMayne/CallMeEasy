27478f506af8aff4dccfaf60861ef761bfb95459
Byron Mayne <Byronmayne@gmail.com>
2016-08-05 12:51:35 -0400

    First commit of Call Me Easy and just started to make some basic attributes.

---
+++ Assets/Call Me Easy/Attributes/OnAssetImportedAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// This attribute is called whenever an asset is imported into Unity. Will be invoked
+/// once per asset. 
+/// <remarks>Only invoked in the Editor</remarks>
+/// <function_signature> void (string assetPath)</function_signature>
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public class OnAssetImportedAttribute : PropertyAttribute
+{
+	public OnAssetImportedAttribute()
+	{
+	}
+}
+
+/// <summary>
+/// The method applied to this attribute will be called whenever an asset
+/// of type <T> is imported to Unity. 
+/// <function_signature> void (string assetPath)</function_signature>
+/// <remarks>Only invoked in the Editor</remarks>
+/// </summary>
+public class OnAssetImportedAttribute<T> : OnAssetImportedAttribute wher T : UnityEngine.Object
+{
+	public OnAssetImportedAttribute() : base() {} 
+}
---
+++ Assets/Call Me Easy/Attributes/OnBuildStartedAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// This attribute is called whenever an asset is imported into Unity. Will be invoked
+/// once per asset. 
+/// <function_signature> void ()</function_signature>
+/// <function_signature> void (string[] levels, string locationPathName, BuildTarget target, BuildOptions options, out uint crc)</function_signature>
+/// <remarks>Only invoked in the Editor</remarks>
+/// <remarks>Does not work in injected callbacks is disabled.</remarks>
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public class OnBuildStarted : PropertyAttribute
+{
+	public OnBuildStarted()
+	{
+
+	}
+}
